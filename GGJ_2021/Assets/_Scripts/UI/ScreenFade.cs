74ccc36062a106d2847459db978f5573b4dee284
Joel Lee <lee.joel395@gmail.com>
2021-01-30 18:06:19 -0800

    Added in end of level art slideshow transition. Loads next level(in the build index) when out of images.

--- GGJ_2021/Assets/_Scripts/UI/ScreenFade.cs
+++ GGJ_2021/Assets/_Scripts/UI/ScreenFade.cs
@@ -8,11 +8,21 @@ public class ScreenFade : MonoBehaviour
 {
     [SerializeField]
     private Image _fadeScreen;
-    private Color alphaB, opaqueB;
+    [SerializeField]
+    private Image _illustrationContainer;
+    [SerializeField]
+    private Sprite[] _illustrations;
+
+
+    private Color alphaB, opaqueB, alphaW, opaqueW;
+    private int cycleIndex = 0;
 
     private void OnEnable() {
         alphaB = opaqueB = Color.black;
         alphaB.a = 0;
+        alphaW = opaqueW = Color.white;
+        alphaW.a = 0;
+
         _fadeScreen.color = opaqueB;
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
@@ -28,6 +38,36 @@ public class ScreenFade : MonoBehaviour
 
     public void DoFadeOut(Action callback = null){
         _fadeScreen.raycastTarget = true;
+        Color start = _fadeScreen.color;
         this.DoTween(lerp => { _fadeScreen.color = Color.Lerp(alphaB, opaqueB, lerp); }, callback, 1.25f, easeType: EaseType.easeOutCubic);
     }
+
+    /// <summary>
+    /// Triggered by handing items into grandma
+    /// </summary>
+    public void FadeArtOut() {
+        //out of pics, load next level directly
+        if (cycleIndex > _illustrations.Length) {
+            DoFadeOut(() => { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); });
+        }
+
+        //transparent to solid white
+        Color start = _fadeScreen.color;
+        _fadeScreen.raycastTarget = true;
+        this.DoTween(lerp => { _fadeScreen.color = Color.Lerp(start, opaqueW, lerp); },
+        ()=> {
+            _illustrationContainer.gameObject.SetActive(true);
+            FadeArtOut();
+            _illustrationContainer.sprite = _illustrations[cycleIndex];
+            cycleIndex++;
+        }, duration: 1.0f, easeType: EaseType.linear, useUnscaledTime: true);
+    }
+
+    public void FadeArtIn() {
+        Color start = _fadeScreen.color;
+        this.DoTween(lerp => { _fadeScreen.color = Color.Lerp(start, alphaW, lerp); }, ()=> { _fadeScreen.raycastTarget = false; }, duration: 1.0f, easeType: EaseType.linear, useUnscaledTime: true);
+
+    }
+
+
 }
