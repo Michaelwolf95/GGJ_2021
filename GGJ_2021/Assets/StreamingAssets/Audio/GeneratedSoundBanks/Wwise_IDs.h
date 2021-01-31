ac09c4fc3b789f4685bfcbdcc89b887b6b48b132
nightsharks <mandoyastar@gmail.com>
2021-01-31 11:18:33 -0800

    [audio] added objs to seq containers

--- GGJ_2021/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ GGJ_2021/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -22,10 +22,8 @@ namespace AK
         static const AkUniqueID PLAY_CROW_WING_FLAP_LARGE = 1074723603U;
         static const AkUniqueID PLAY_CROW_WING_FLAP_MED = 550970928U;
         static const AkUniqueID PLAY_MUS_MAIN_GAME_LP = 3265397713U;
-        static const AkUniqueID PLAY_OBJ_DROP_GENERIC = 1568976433U;
-        static const AkUniqueID PLAY_OBJ_DROP_SHINY = 4281314467U;
-        static const AkUniqueID PLAY_OBJ_PICKUP_GENERIC = 3741681902U;
-        static const AkUniqueID PLAY_OBJ_PICKUP_SHINY = 1433800392U;
+        static const AkUniqueID PLAY_OBJ_GENERIC_INTERACT = 1281393260U;
+        static const AkUniqueID PLAY_OBJ_SHINY_INTERACT = 3109608386U;
         static const AkUniqueID PLAY_OBJ_SHINY_OBJECT_LP = 1065595628U;
         static const AkUniqueID PLAY_UI_BUTTON_HOVER_GENERIC = 155552906U;
         static const AkUniqueID PLAY_UI_BUTTON_PRESS_GENERIC = 750236145U;
