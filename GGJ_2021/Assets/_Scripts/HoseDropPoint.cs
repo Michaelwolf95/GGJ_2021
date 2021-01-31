3eecd402475939ab0980bee380506043b999dea6
Michael Wolf <michaelwolf95@gmail.com>
2021-01-31 13:30:16 -0800

    Hose for hoes and crows.

--- GGJ_2021/Assets/_Scripts/HoseDropPoint.cs
+++ GGJ_2021/Assets/_Scripts/HoseDropPoint.cs
@@ -11,17 +11,27 @@ public class HoseDropPoint : MonoBehaviour
     public ObiParticleAttachment headPoint;
     public Animation coinAnimation;
 
+    public InteractableReticle reticle;
+
+    private bool used = false;
+    
     public void OnTriggerEnter(Collider other) {
+        if(used) return;
         if(other.gameObject.name == "Player" && _playerInteractor.heldObject != null) {
             _interactObject = _playerInteractor.heldObject;
             _playerInteractor.onFinishInteractionEvent += DropHose;
+            
+            reticle.ToggleInteractReticle(true);
         }
     }
 
     public void OnTriggerExit(Collider other) {
+        if(used) return;
         if (other.gameObject.name == "Player") {
             _interactObject = null;
             _playerInteractor.onFinishInteractionEvent -= DropHose;
+            
+            reticle.ToggleInteractReticle(false);
         }
     }
 
@@ -32,5 +42,10 @@ public class HoseDropPoint : MonoBehaviour
         coinAnimation.Play("coinAnimation");
         _interactObject.transform.position = Vector3.zero; // banish this
         _interactObject.gameObject.SetActive(false);
+        
+        reticle.ToggleInteractReticle(false);
+        
+        _playerInteractor.onFinishInteractionEvent -= DropHose;
+        used = true;
     }
 }
