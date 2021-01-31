614d755f40406a53bef686f8e612c66e84c6e70a
Joel Lee <lee.joel395@gmail.com>
2021-01-31 05:52:45 -0800

    Hose to anchor now

--- GGJ_2021/Assets/_Scripts/HoseDropPoint.cs
+++ GGJ_2021/Assets/_Scripts/HoseDropPoint.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Obi;
 
 public class HoseDropPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private Interactor _playerInteractor;
+    private GrabObject _interactObject;
+
+    public GameObject goalPointMesh;
+    public ObiParticleAttachment headPoint;
+
+    public void OnTriggerEnter(Collider other) {
+        if(other.gameObject.name == "Player") {
+            _interactObject = _playerInteractor.heldObject;
+            _playerInteractor.onFinishInteractionEvent += DropHose;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == "Player") {
+            _interactObject = null;
+            _playerInteractor.onFinishInteractionEvent -= DropHose;
+        }
+    }
+
+    private void DropHose() {
+        if(_interactObject == null || headPoint == null) { return; }
+        headPoint.target = this.transform;
+        goalPointMesh.SetActive(true);
+        _playerInteractor.QuitInteraction();
+        _interactObject.gameObject.SetActive(false);
     }
 }
